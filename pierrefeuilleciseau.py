ad01bb2913b2848534fc35ad8b533d6db0641154
pumbaa <gbravin69@gmail.com>
2022-11-24 17:33:16 +0100

    final avec easter egg

--- pierrefeuilleciseau.py
+++ pierrefeuilleciseau.py
@@ -5,7 +5,15 @@ def PierreFeuilleCiseau(pfc_joueur):
     pfc_ordi=random.randint(0,2)
     if pfc_ordi==pfc_joueur:
         print("égalité !")
+        return
     elif (pfc_ordi==0 and pfc_joueur==2) or (pfc_ordi==1 and pfc_joueur==0) or (pfc_ordi==2 and pfc_joueur==1):
         print("perdu !")
+        return
     elif (pfc_ordi==2 and pfc_joueur==0) or (pfc_ordi==0 and pfc_joueur==1) or (pfc_ordi==1 and pfc_joueur==2):
         print("gagné !")
+        return
+
+jouer=int(input("pierre = 0 | feuille = 1 | ciseau = 2"))
+PierreFeuilleCiseau(jouer)
+
+    
