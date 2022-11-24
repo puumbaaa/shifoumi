319da32ee5d51a66427c1bed8aacbbe0581399b7
pumbaa <gbravin69@gmail.com>
2022-11-24 19:25:19 +0100

    commentaire

--- morpion.py
+++ morpion.py
@@ -82,12 +82,14 @@ def morpion2player():
                     print("case déja utilisé")
                     print('rejouer')
             displaytab(tabmorpion1)
-
+# dictionnaire du tableau du morpion joueur contre ordi
 tabmorpion = {1: ' ', 2: ' ', 3: ' ',
               4: ' ', 5: ' ', 6: ' ',
               7: ' ', 8: ' ', 9: ' '}
+# joueur a le signe O et ordi a le signe X
 joueur = 'O'
 ordi = 'X'
+#fonction qui affiche le tableau du morpion joueur contre ordi
 def afficherTable(tabmorpion):
     print(tabmorpion[1] + '|' + tabmorpion[2] + '|' + tabmorpion[3])
     print('-+-+-')
@@ -96,22 +98,26 @@ def afficherTable(tabmorpion):
     print(tabmorpion[7] + '|' + tabmorpion[8] + '|' + tabmorpion[9])
     print("\n")
 
-
+# fonction qui vérifie si la case est prise ou non
 def espaceVide(position):
     if tabmorpion[position] == ' ':
         return True
     else:
         return False
 
-
+#fonction qui insert le signe dans la position demandé
 def insertLetter(signe, position):
+    #si la case n'est pas prise
     if espaceVide(position):
+        #inserer le signe dans le tableau
         tabmorpion[position] = signe
         afficherTable(tabmorpion)
+        # si égalité
         if (verificationEgalite()):
             print("égalité !")
             return
-        if verificationVictoire():
+        #si victoire du joueur ou de l'ordi
+        elif verificationVictoire():
             if signe == 'X':
                 print("perdu !!")
                 return
@@ -121,70 +127,89 @@ def insertLetter(signe, position):
 
         return
 
-
+    # si la case est prise redemander la case
     else:
         print("déjà prise !")
         position = int(input("entrer nouvelle position:  "))
         insertLetter(signe, position)
         return
 
-
+# fonction qui vérifie la victoire d'un joueur ou de l'ordi sans vérifier qui gagne
 def verificationVictoire():
+    # ligne 1
     if (tabmorpion[1] == tabmorpion[2] and tabmorpion[1] == tabmorpion[3] and tabmorpion[1] != ' '):
         return True
+    # ligne 2
     elif (tabmorpion[4] == tabmorpion[5] and tabmorpion[4] == tabmorpion[6] and tabmorpion[4] != ' '):
         return True
+    # ligne 3
     elif (tabmorpion[7] == tabmorpion[8] and tabmorpion[7] == tabmorpion[9] and tabmorpion[7] != ' '):
         return True
+    # colonne 1
     elif (tabmorpion[1] == tabmorpion[4] and tabmorpion[1] == tabmorpion[7] and tabmorpion[1] != ' '):
         return True
+    # colonne 2
     elif (tabmorpion[2] == tabmorpion[5] and tabmorpion[2] == tabmorpion[8] and tabmorpion[2] != ' '):
         return True
+    # colonne 3
     elif (tabmorpion[3] == tabmorpion[6] and tabmorpion[3] == tabmorpion[9] and tabmorpion[3] != ' '):
         return True
+    # diagonale 1
     elif (tabmorpion[1] == tabmorpion[5] and tabmorpion[1] == tabmorpion[9] and tabmorpion[1] != ' '):
         return True
+    # diagonale 2
     elif (tabmorpion[7] == tabmorpion[5] and tabmorpion[7] == tabmorpion[3] and tabmorpion[7] != ' '):
         return True
+    # sinon
     else:
         return False
 
-
+#vérification du gagnant
 def verificationGagnant(signe):
+    # ligne 1
     if tabmorpion[1] == tabmorpion[2] and tabmorpion[1] == tabmorpion[3] and tabmorpion[1] == signe:
         return True
+    # ligne 2
     elif (tabmorpion[4] == tabmorpion[5] and tabmorpion[4] == tabmorpion[6] and tabmorpion[4] == signe):
         return True
+    # ligne 3
     elif (tabmorpion[7] == tabmorpion[8] and tabmorpion[7] == tabmorpion[9] and tabmorpion[7] == signe):
         return True
+    # colonne 1
     elif (tabmorpion[1] == tabmorpion[4] and tabmorpion[1] == tabmorpion[7] and tabmorpion[1] == signe):
         return True
+    # colonne 2
     elif (tabmorpion[2] == tabmorpion[5] and tabmorpion[2] == tabmorpion[8] and tabmorpion[2] == signe):
         return True
+    # colonne 3
     elif (tabmorpion[3] == tabmorpion[6] and tabmorpion[3] == tabmorpion[9] and tabmorpion[3] == signe):
         return True
+    # diagonale 1
     elif (tabmorpion[1] == tabmorpion[5] and tabmorpion[1] == tabmorpion[9] and tabmorpion[1] == signe):
         return True
+    # diagonale 2
     elif (tabmorpion[7] == tabmorpion[5] and tabmorpion[7] == tabmorpion[3] and tabmorpion[7] == signe):
         return True
+    # sinon
     else:
         return False
 
-
+# fonction qui vérifie une égalité
 def verificationEgalite():
     for key in tabmorpion.keys():
         if (tabmorpion[key] == ' '):
             return False
     return True
 
-
+# fonction qui demande au joueur de choisir sa case
 def choixJoueur():
     position = int(input("entrer position pour 'O':  "))
     insertLetter(joueur, position)
     return
 
-
+# fonction récursive qui permet le choix de l'ordi
 def choixOrdi():
+    # définition des variables meilleurScore et meilleurCoup qui définie un score à chaque coup de base très bas 
     meilleurScore = -800
     meilleurCoup = 0
     for key in tabmorpion.keys():
@@ -229,16 +254,20 @@ def minimax(tabmorpion, profondeur, isMaximizing):
                 if (score < bestScore):
                     bestScore = score
         return bestScore
-
+# fonction qui permet de jouer au pierre feuille ciseau contre un ordi
 def PierreFeuilleCiseau(pfc_joueur):
     pfc=["pierre","feuille","ciseau"]
+    # le coup jouer par l'ordi
     pfc_ordi=random.randint(0,2)
+    # vérification égalité
     if pfc_ordi==pfc_joueur:
         print("égalité !")
         return
+    # vérification défaite
     elif (pfc_ordi==0 and pfc_joueur==2) or (pfc_ordi==1 and pfc_joueur==0) or (pfc_ordi==2 and pfc_joueur==1):
         print("perdu !")
         return
+    # vérification défaite
     elif (pfc_ordi==2 and pfc_joueur==0) or (pfc_ordi==0 and pfc_joueur==1) or (pfc_ordi==1 and pfc_joueur==2):
         print("gagné !")
         return
